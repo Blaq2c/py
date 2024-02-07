771bbaa45f4da982e04b9dd5c92a3ef5121ede6d
Job <t.mwangim02@gmail.com>
2024-02-07 10:41:23 +0300

    py

---
+++ main.py
@@ -0,0 +1,12 @@
+print(f'20 days are {50} minutes')
+'''
+string concantenation method
+'''
+print('20 days are ' + str(50) + ' minutes')
+#normal concantenation method
+'''
+Calculate no of minutes in 20 days
+'''
+print(f'20 days are {20 * 24 * 60} minutes')
+print('20 days are ' + str(20 * 24 * 60) + ' minutes')
+
